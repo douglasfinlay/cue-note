32a754f273f5ba96b94381f90208a8ae7fb86539
Douglas Finlay <douglas@douglasfinlay.com>
2023-02-15 18:44:53 +0000

    Add submit and clear buttons to note input

--- src/App.tsx
+++ src/App.tsx
@@ -199,6 +199,35 @@ function App() {
                         onEnterPressed={saveNote}
                         onTextChanged={onEditNoteTextEdited}
                     />
+                    <div className='flex flex-col gap-2'>
+                        <button
+                            className='grow-0 shrink-0 px-3 w-24 h-12 rounded bg-eos-grey-dark border-2 border-solid border-eos-grey-light disabled:opacity-50'
+                            disabled={!readyToNote || !editingCue}
+                            onClick={discardNote}
+                        >
+                            <div className='leading-5'>Cancel</div>
+                            <div className='text-xs'>(Esc)</div>
+                        </button>
+                        <button
+                            className='grow rounded px-3 w-24 h-12 bg-eos-grey-dark border-2 border-solid border-eos-grey-light disabled:opacity-50'
+                            disabled={!readyToNote || !editingCue}
+                            onClick={saveNote}
+                        >
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='24'
+                                height='24'
+                                fill='currentColor'
+                                className='bi bi-arrow-return-left inline-block'
+                                viewBox='0 0 16 16'
+                            >
+                                <path
+                                    fillRule='evenodd'
+                                    d='M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z'
+                                />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className='basis-1/3 grow-0 shrink-0 flex gap-2 flex-col'>
