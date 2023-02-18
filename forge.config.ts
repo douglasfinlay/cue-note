a7ff72dd86a7c945b629ee160a17007287736f02
Douglas Finlay <douglas@douglasfinlay.com>
2023-02-18 23:27:40 +0000

    Add Github publisher

--- forge.config.ts
+++ forge.config.ts
@@ -1,7 +1,8 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerDMG } from '@electron-forge/maker-dmg';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import { PublisherGithub } from '@electron-forge/publisher-github';
+import type { ForgeConfig } from '@electron-forge/shared-types';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -12,6 +13,15 @@ const config: ForgeConfig = {
     makers: [
         new MakerSquirrel({}),
         new MakerDMG({}),
+    ],
+    publishers: [
+        new PublisherGithub({
+            repository: {
+                owner: 'douglasfinlay',
+                name: 'cue-note',
+            },
+            draft: true
+        }),
     ],
     plugins: [
         new WebpackPlugin({
